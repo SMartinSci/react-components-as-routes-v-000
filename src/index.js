fb0acd2d5128de1e3eb0228e23b1723452b35827
Sherry Martin <sm674@cornell.edu>
2019-01-31 19:06:28 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -26,6 +26,17 @@ const Login = () => {
           <input type="text" name="username" placeholder="Username" />
           <label htmlFor="username">Username</label>
 
+</div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
+
 ReactDOM.render(
   <Router>
     <Route path="/" render={Home} />
