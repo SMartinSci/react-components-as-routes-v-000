a8e291443a5de86ff0218f47d518ec81d098aa27
Sherry Martin <sm674@cornell.edu>
2019-01-31 19:06:07 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -10,6 +10,15 @@ const Home = () => {
   );
 };
 
+const About = () => {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+};
+ 
+
 ReactDOM.render(
   <Router>
     <Route path="/" render={Home} />
