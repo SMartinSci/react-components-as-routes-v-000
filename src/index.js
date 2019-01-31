ca38d2b7a8b0462b05d0cfacc0023f9d2d8ef07b
Sherry Martin <sm674@cornell.edu>
2019-01-31 19:08:14 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+/* Add NavLink to importer */
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
